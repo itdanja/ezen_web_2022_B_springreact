48ef20eeeb22732dd373fe6652b7a74fdabc8ac1
이젠강사 <itdanja@kakao.com>
2023-04-17 16:31:14 +0900

    day73 각 리포지토리 생성

---
+++ src/main/java/ezenweb/web/domain/board/BoardEntityRepository.java
@@ -0,0 +1,7 @@
+package ezenweb.web.domain.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardEntityRepository extends JpaRepository< BoardEntity , Integer > { }
