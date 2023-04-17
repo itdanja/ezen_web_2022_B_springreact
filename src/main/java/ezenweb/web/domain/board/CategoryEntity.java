c37a737916d277503ce956626e577dba99b143b2
이젠강사 <itdanja@kakao.com>
2023-04-17 15:50:08 +0900

    day73 JPA 조인테이블 양방향 구현

--- src/main/java/ezenweb/web/domain/board/CategoryEntity.java
+++ src/main/java/ezenweb/web/domain/board/CategoryEntity.java
@@ -21,7 +21,7 @@ public class CategoryEntity {
     // 양방향
     // 카테고리[pk] <----> 게시물[fk]
     // pk테이블에는 fk 흔적 남긴적이 없다.[필드 존재x 객체 존재o ]
-    @OneToMany( mappedBy =  "memberEntity") // 하나 가 다수에게 [ pk --> fk ]
+    @OneToMany( mappedBy =  "categoryEntity") // 하나 가 다수에게 [ pk --> fk ]
     @Builder.Default // .builder 사용시 현재 필드 기본값으로 설정
     private List<BoardEntity> boardEntityList = new ArrayList<>();
 
