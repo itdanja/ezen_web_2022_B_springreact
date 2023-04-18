50f89512e38adb613f38e2716fcd6655bac8e8d0
MSI <kgs2072@naver.com>
2023-04-19 01:07:25 +0900

    0419

--- src/main/frontend/src/AddTodo.js
+++ src/main/frontend/src/AddTodo.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React , { useState } from 'react';
 
-import { Button, Grid, TextField } from "@mui/material";
+import {
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+  Paper,
+  List ,
+  Container ,
+  Grid ,
+  TextField,Button
+} from "@mui/material";
 
-const AddTodo = (props) => {
-    // 사용자의 입력을 저정할 오브젝트
-    const [item, setItem] = useState({ title: ""});
+export default function AddTodo( props ){
+
+    const [ item , setItem ] = useState( {title : "" } );
     const addItem = props.addItem;
 
     // onButtonClick 함수 작성
@@ -13,28 +25,27 @@ const AddTodo = (props) => {
         setItem({ title: "" });
     };
 
-    // onInputChange 함수 작성
-    const onInputChange = (e) => {
-        setItem({title: e.target.value});
-        console.log(item);
-    };
+        // onInputChange 함수 작성
+        const onInputChange = (e) => {
+            setItem({title: e.target.value});
+            console.log(item);
+        };
 
-    // enterKeyEventHandler 함수
-    const enterKeyEventHandler = (e) => {
-        if (e.key === 'Enter') {
-          onButtonClick();
-        }
-    };
+            // enterKeyEventHandler 함수
+            const enterKeyEventHandler = (e) => {
+                if (e.key === 'Enter') {
+                  onButtonClick();
+                }
+            };
 
 
-    // onInputChange 함수 TextField에 연결
-    return (
+    return(
         <Grid container style={{ marginTop: 20 }}>
           <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
             <TextField placeholder="Add Todo here" fullWidth
-            onChange={onInputChange} 
-            onKeyPress={enterKeyEventHandler}
-            value={item.title}/>
+                        onChange={onInputChange}
+                        onKeyPress={enterKeyEventHandler}
+                        value={item.title}/>
           </Grid>
           <Grid xs={1} md={1} item >
             <Button fullWidth style={{ height: '100%' }} color="secondary" variant="outlined"
@@ -44,6 +55,6 @@ const AddTodo = (props) => {
           </Grid>
         </Grid>
     );
-}
 
-export default AddTodo;
+
+}
--- src/main/frontend/src/Todo.js
+++ src/main/frontend/src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React , { useState } from 'react';
 import {
   ListItem,
   ListItemText,
@@ -6,69 +6,76 @@ import {
   Checkbox,
   ListItemSecondaryAction,
   IconButton,
-} from "@mui/material";
-import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
+} from "@mui/material"; // npm install @mui/material @emotion/react @emotion/styled
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined"; // npm install @mui/icons-material
 
-const Todo = (props) => {
-  const [item, setItem] = useState(props.item);
-  const [readOnly, setReadOnly] = useState(true);
-  const deleteItem = props.deleteItem;
-  const editItem = props.editItem;
+export default function Todo( props ){
 
-  const editEventHandler = (e) => {
-    item.title = e.target.value;
-    editItem();
-  };
+    const [ item , setItem ] = useState( props.item );
+     const deleteItem = props.deleteItem;
+     const [readOnly, setReadOnly] = useState(true);
+       const editItem = props.editItem;
 
-  const checkboxEventHandler = (e) => {
-    item.done = e.target.checked;
-    editItem();
-  }
 
-  // deleteEventHandler 작성
-  const deleteEventHandler = () => {
-    deleteItem(item);
-  };
 
-  const turnOffReadOnly = () => {
-    setReadOnly(false);
-  }
+       // deleteEventHandler 작성
+       const deleteEventHandler = () => {
+         deleteItem(item);
+       };
 
-  // turnOnReadOnly 함수 작성
-  const turnOnReadOnly = (e) => {
-    if (e.key === "Enter") {
-        setReadOnly(true);
-    }
-  };
+        const turnOffReadOnly = () => {
+           setReadOnly(false);
+         }
 
-  return (
-    <ListItem>
-      <Checkbox checked={item.done}
-      onChange={checkboxEventHandler} />
-      <ListItemText>
-        <InputBase
-          inputProps={{ 
-              "aria-label": "naked",
-              readOnly: readOnly }}
-          onClick={turnOffReadOnly}
-          onKeyDown={turnOnReadOnly}
-          onChange={editEventHandler}
-          type="text"
-          id={item.id}
-          name={item.id}
-          value={item.title}
-          multiline={true}
-          fullWidth={true}
-        />
-      </ListItemText>
-      <ListItemSecondaryAction>
-        <IconButton aria-label="Delete Todo"
-          onClick={deleteEventHandler} >
-          <DeleteOutlined />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  );
+           // turnOnReadOnly 함수 작성
+           const turnOnReadOnly = (e) => {
+             if (e.key === "Enter") {
+                 setReadOnly(true);
+             }
+           };
+
+             const editEventHandler = (e) => {
+               item.title = e.target.value;
+               editItem();
+             };
+
+               const checkboxEventHandler = (e) => {
+                 item.done = e.target.checked;
+                 editItem();
+               }
+
+
+
+
+
+    return (
+        <ListItem>
+                  <Checkbox checked={item.done}
+                  onChange={checkboxEventHandler} />
+            <ListItemText>
+                <InputBase
+                          inputProps={{
+                              "aria-label": "naked",
+                              readOnly: readOnly }}
+                          onClick={turnOffReadOnly}
+                          onKeyDown={turnOnReadOnly}
+                           onChange={editEventHandler}
+                          type="text"
+                          id={item.id}
+                          name={item.id}
+                          value={item.title}
+                          multiline={true}
+                          fullWidth={true}
+                        />
+            </ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete Todo"
+                      onClick={deleteEventHandler} >
+                      <DeleteOutlined />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+
+
+        </ListItem>
+    );
 };
-
-export default Todo;
