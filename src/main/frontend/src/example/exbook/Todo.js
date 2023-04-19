6d3030c99f21fc35c2ce24ed1d28d61fd3b2f062
이젠강사 <itdanja@kakao.com>
2023-04-19 14:00:44 +0900

    day75 컴포넌트에 css 적용하는방법

---
+++ src/main/frontend/src/example/exbook/Todo.js
@@ -0,0 +1,17 @@
+// p. 183 컴포넌트 만들기
+import React from 'react';
+
+export default function Todo(props) {
+    return ( <>
+        <div className="Todo">
+            <input
+                type="checkbox"
+                id="todo0"
+                name="todo0"
+                value="todo0"
+            />
+            <label for="todo0"> Toto 컴포넌트 만들기 <label>
+        </div>
+    </>);
+
+}
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -9,13 +9,28 @@ import Book from './example/ex1component/Book';
 import Product from './example/ex1component/Product';
 import ProductList from './example/ex1component/ProductList';
 import Clock from './example/ex1component/Clock';
-
+import CommentList from './example/ex2css/CommentList';
 
 //1. HTML에 존재하는 div 가져오기 [ document.getElementById('root') ]
 //2. ReactDOM.createRoot( 해당 div ) : 해당 div를 리액트 root 로 사용하여 root 객체 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// 4. 예제4 렌더링 반복
+// 5. 예제5 css 적용
+root.render(
+    <React.StrictMode>
+        <CommentList />
+    </React.StrictMode>
+)
+
+
+
+
+
+
+
+
+
+/*// 4. 예제4 렌더링 반복
 // 1초마다 해당 코드 실행 : setInterval( () => { } , 1000 );
 setInterval( () => {
     root.render(
@@ -23,9 +38,7 @@ setInterval( () => {
             <Clock />
         </React.StrictMode>
     );
-} , 1000 );
-
-
+} , 1000 );*/
 
 // 3. 예제3 컴포넌트에 컴포넌트 포함 하기
 /*root.render(
