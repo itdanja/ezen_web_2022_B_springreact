d18c577b14d749730fb674843cdf7df4942964e3
이젠강사 <itdanja@kakao.com>
2023-04-19 11:31:06 +0900

    day75 컴포넌트 만들어서 싱글페이지에 렌더링 하기

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';    //*1.import 이용해 App 컴포넌트[함수]를 불러온다.
 import reportWebVitals from './reportWebVitals';
+// ---------------------------------------------------------------- //
+// import 컴포넌트명 from 'index.js파일 기준으로 상대경로'
+import Book from './example/ex1component/Book';
 
+//1. HTML에 존재하는 div 가져오기 [ document.getElementById('root') ]
+//2. ReactDOM.createRoot( 해당 div ) : 해당 div를 리액트 root 로 사용하여 root 객체 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 1. 예제1 개발자 정의 컴포넌트 렌더링
+root.render(
+    <React.StrictMode>
+        <Book />
+    </React.StrictMode>
+);
+
+/* 기본값
+// 3. root.render( ) : 해당 root객체( 우리가 가져온 div )의 컴포넌트 렌더링
 root.render(
   <React.StrictMode>
-    <App />
+    // *2. <컴포넌트 /> 을 이용해 컴포넌트를 사용한다.
+    <App /> // 4. App 컴포넌트에 render 함수에 포함 [ App 호출하는 방법 : 상단에 import ]
   </React.StrictMode>
 );
+*/
+
+// ------------------------------- vs --------------------------//
+// 1. 리액트 사용하기전 직접 렌더링
+/*
+document.querySelector('#root').innerHTML =
+    `
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    `
+*/
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
