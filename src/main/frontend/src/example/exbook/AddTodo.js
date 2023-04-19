6ad784b03275c759ab64ecd41f8f7751f45f2e57
이젠강사 <itdanja@kakao.com>
2023-04-19 16:10:07 +0900

    day75 입력받은 값을 상태변수에 저장하기

--- src/main/frontend/src/example/exbook/AddTodo.js
+++ src/main/frontend/src/example/exbook/AddTodo.js
@@ -4,10 +4,26 @@ import { Button , Grid , TextField } from '@mui/material'
 export default function AddTodo( props ){
     // 사용자가 입력한 데이터를 저장할 상태변수
     const [item , setItem ] = useState( {title : "" } )
+
+    //1. 사용자가 입력한 데이터를 가져오는 함수
+    // onChange={ onInputChange }
+    // onChange 이벤트속성 : 해당 요소가 해당 이벤트를 발생했을때 이벤트 정보를 반환[ e ]
+    // onChange이벤트정보 e를  onInputChange 함수로 매개변수 e 전달
+    const onInputChange = ( e )=>{
+            // console.log( e ); // 해당 이벤트가 발생했을때의 이벤트정보 들어있는 객체
+            // console.log( e.target ); // 해당 이벤트가 발생된 요소 [ TextField ]
+        setItem( { title : e.target.value } );  // 상태변경 : set~~~  : 입력받은 값을 가져와서 상태변수를 수정
+            console.log( item );
+    }
+
     return (<>
         <Grid container style={{ marginTop : 20 }}>
             <Grid xs={11} md={11} item style={{ paddingRight:16}}>
-                    <TextField placeholder="여기에 Todo 작성" fullWidth />
+                    <TextField
+                        placeholder="여기에 Todo 작성"
+                        fullWidth
+                        onChange={ onInputChange }
+                    />
             </Grid>
             <Grid xs={1} md={1} item >
                 <Button fullWidth style={{ height : '100%' }} color="secondary" variant="outlined">
@@ -16,5 +32,4 @@ export default function AddTodo( props ){
             </Grid>
         </Grid>
     </>)
-
 }
