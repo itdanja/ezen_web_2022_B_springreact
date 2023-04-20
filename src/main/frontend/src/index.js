2c028975906091d262743f9f5cfb1dcf30e0bc66
이젠강사 <itdanja@kakao.com>
2023-04-20 14:07:15 +0900

    day76 Hook useState , useEffect

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -10,18 +10,32 @@ import Product from './example/ex1component/Product';
 import ProductList from './example/ex1component/ProductList';
 import Clock from './example/ex1component/Clock';
 import CommentList from './example/ex2css/CommentList';
+import Hook1 from './example/ex3hook/Hook1';
+import Hook2 from './example/ex3hook/Hook2';
+
 import AppTodo from './example/exbook/AppTodo';
 
 //1. HTML에 존재하는 div 가져오기 [ document.getElementById('root') ]
 //2. ReactDOM.createRoot( 해당 div ) : 해당 div를 리액트 root 로 사용하여 root 객체 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+/*
 // ----- . 교재 Todo 컴포넌트 적용
 root.render(
     <React.StrictMode>
         <AppTodo />
     </React.StrictMode>
 )
+*/
+
+
+// ----- . 훅 예제
+root.render(
+    <React.StrictMode>
+        <Hook2 />
+    </React.StrictMode>
+)
+
 
 
 /*// 5. 예제5 css 적용
