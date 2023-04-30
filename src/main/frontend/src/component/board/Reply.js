0625fccb1cfb9670406a7ab3f49a811d53f69504
MSI <kgs2072@naver.com>
2023-05-01 03:01:26 +0900

    0501

---
+++ src/main/frontend/src/component/board/Reply.js
@@ -0,0 +1,23 @@
+import React,{ useState , useEffect } from 'react';
+import styles from '../../css/board/board.css';
+
+export default function Reply( props ) {
+      const [comment, setComment] = useState('');
+
+       const handleCommentChange = (event) => {
+         setComment(event.target.value);
+       };
+
+       const handleAddComment = () => {
+            props.onReplyWrite( comment , props.rindex );
+            setComment('');
+       };
+  return <div>
+          <div className="replyInput">
+                <textarea value={comment} onChange={handleCommentChange} className="comment" />
+                <button className="commentBtn" onClick={handleAddComment}>댓글작성</button>
+          </div>
+  </div>;
+};
+
+
