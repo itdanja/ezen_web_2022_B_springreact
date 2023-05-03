46da8a9616593d1b8438992948b069b77f74436f
이젠강사 <itdanja@kakao.com>
2023-05-03 11:34:44 +0900

    day85 useRef() 훅 사용

--- src/main/frontend/src/component/board/Chatting.js
+++ src/main/frontend/src/component/board/Chatting.js
@@ -3,18 +3,41 @@ import Container from '@mui/material/Container';
 
 export default function Chatting(props){
 
-    // 1. 웹소켓 = webSocket = JS
+    let 숫자 = 10;                            // 지역변수 : 컴포넌트[함수] 호출 될때마다 초기화
+    let 숫자2 = useRef(10);                   // 재렌더링시 초기값이 적용되지 않는 함수 [ 반환값 : 객체{ current : 값 } ]
+    let inputRef = useRef(null);            // document.querySelector vs useRef
+
+    const [ id , setId ] = useState(0); // set메소드 사용시 컴포넌트 재호출[재렌더링]
+
+/*    // 1. 웹소켓 = webSocket = JS
     let webSocket = useRef( null );
     // 2.
     useEffect( () => {
         if( !webSocket.current ){
             webSocket.current = new webSocket('ws://localhost:8080/chat');
-
             // 1. 웹소켓이 접속 성공했을때.
             webSocket.current.onopen = () =>  { console.log(webSocket);  }
         }
     })
+*/
 
+    console.log( 숫자 )
+    console.log( 숫자2 )
+    console.log( id )
+
+    // 2. 난수 생성
+    let randId = Math.floor( Math.random() * ( 9999 -1 ) +1 );
+        // Math.floor( Math.random() * ( 최댓값 - 최소값 ) + 최소값 ); : 정수 1~1000
+    숫자 = randId;
+    숫자2.current = randId;
+
+    // setId( randId );
+
+    const onSend = () =>{
+        console.log( inputRef.current.value  )
+        console.log( document.querySelector('.msgInput').value )
+        inputRef.current.value = 3543;
+    }
     return (<>
         <Container>
            <h6>익명 채팅방</h6>
@@ -23,8 +46,8 @@ export default function Chatting(props){
            </div>
            <div className="chatInputBox">
                 <span> 익명94 </span>
-                <input type="text" />
-                <button>전송</button>
+                <input ref={ inputRef } type="text" className="msgInput" />
+                <button onClick={ onSend }>전송</button>
            </div>
         </Container>
     </>)
--- src/main/java/ezenweb/web/socket/WebSocketConfiguration.java
+++ src/main/java/ezenweb/web/socket/WebSocketConfiguration.java
@@ -2,20 +2,22 @@ package ezenweb.web.socket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Configuration // 컴포넌트 등록
+@Configuration // 설정 컴포넌트 등록
+@EnableWebSocket // * WS 프로토콜의 URL 매핑 연결
 public class WebSocketConfiguration implements WebSocketConfigurer {
-
     @Autowired // 컴포넌트에 등록한 클래스 이므로 @Autowired 가능 [ DI ]
     private ChattingHandler chattingHandler;
-
     @Override // 서버소켓 으로 사용되고 있는 클래스 들 등록
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler( chattingHandler , "/chat").setAllowedOrigins("*");
-        // registry.addHandler( 서버소켓 객체 , "서버소켓의 path/URL") : 서버소켓 등록 함수
-            // .setAllowedOrigins("접속허용 도메인") : 해당 서버소켓으로 부터 요청할수 있는 URL/도메인
-            // .setAllowedOrigins("*") : 모든 도메인 요청 가능
     }
 }
+
+
+// registry.addHandler( 서버소켓 객체 , "서버소켓의 path/URL") : 서버소켓 등록 함수
+// .setAllowedOrigins("접속허용 도메인") : 해당 서버소켓으로 부터 요청할수 있는 URL/도메인
+// .setAllowedOrigins("*") : 모든 도메인 요청 가능
