a8c668a7542a4e19928f7b0eed9b6ff6be0b80c3
이젠강사 <itdanja@kakao.com>
2023-05-03 14:57:47 +0900

    day85 익명채팅 CSS

--- src/main/frontend/src/component/board/Chatting.js
+++ src/main/frontend/src/component/board/Chatting.js
@@ -1,18 +1,20 @@
 import React,{ useEffect , useState , useRef } from 'react';
 import Container from '@mui/material/Container';
+import styles from '../../css/board/chatting.css'
 
 export default function Chatting(props){
 
     let [ id , setId ] = useState(''); // 익명채팅에서 사용할 id [ 난수 저장 ]
     let [ msgContent , setMsgContent ] = useState([]); // 현재 채팅중인 메시지 를 저장하는 변수
     let msgInput = useRef(null); // 채팅입력창[input] DOM객체 제어 변수
+
     // 1. 재렌더링 될때마다 새로운 접속
     // let 클라이언트소켓 = new WebSocket("ws/localhost:8080/chat")  ;
     // 2. 재렌더링 될때 데이터 상태 유지
-    let ws = useRef( null ) ;
-    useEffect( ()=>{
-        if( !ws.current ){
-            ws = new WebSocket("ws://localhost:8080/chat")  ;
+    let ws = useRef( null ) ;   // 1.모든 함수 사용할 클라이언트소켓 변수
+    useEffect( ()=>{            // 2. 컴포넌트 실행시 1번만 실행
+        if( !ws.current ){      // 3.클라이언트소켓이 접속이 안되어있을때. [ * 유효성검사 ]
+            ws.current = new WebSocket("ws://localhost:8080/chat") ; // 4. 서버소켓 연결
              // 3. 서버소켓에 접속했을때 이벤트
             ws.current.onopen = () => {   console.log('서버 접속했습니다.');
                 let randId = Math.floor( Math.random() * ( 9999 - 1 ) + 1  );    setId( '익명' + randId );
@@ -26,10 +28,12 @@ export default function Chatting(props){
                 //msgContent.push(e.data);    // 배열에 내용 추가
                 //setMsgContent( [...msgContent] ); // 재 렌더링
                 let data = JSON.parse( e.data );
-                setMsgContent( (msgContent) => [...msgContent , data  ] ); // 재 렌더링
+                setMsgContent( (msgContent)=> [...msgContent , data  ] ); // 재 렌더링
             }
         }
     } );
+
+
     // 4.메시지 전송
     const onSend = () =>{
         // msgInput변수가 참조중인 <input ref={ msgInput } > 해당 input 를 DOM객체로 호출
@@ -42,19 +46,30 @@ export default function Chatting(props){
         msgInput.current.value = '';
     }
 
+    // 5. 메시지 받기 렌더링 할때마다 스크롤 가장 하단으로 내리기
+    useEffect ( () => {
+        document.querySelector('.chatContentBox').scrollTop = document.querySelector('.chatContentBox').scrollHeight;
+    },[msgContent])
+
     return (<>
         <Container>
-           <h6>익명 채팅방</h6>
            <div className="chatContentBox">
            {
                 msgContent.map( (m)=>{
-                    return(<><div> { m } </div></>)
+                    return(<>
+                       {/* 조건 스타일링 : style={ 조건 ? {참일경우} , {거짓일경우} }  */}
+                        <div className="chatContent" style={ m.id == id ? { backgroundColor: '#d46e6e' } : { } }  >
+                            <span> { m.id } </span>
+                            <span> { m.time } </span>
+                            <span> { m.msg } </span>
+                        </div>
+                    </>)
                 })
            }
            </div>
            <div className="chatInputBox">
                 <span> { id }  </span>
-                <input ref={ msgInput } type="text" />
+                <input className="msgInput" ref={ msgInput } type="text" />
                 <button onClick={ onSend }>전송</button>
            </div>
         </Container>
