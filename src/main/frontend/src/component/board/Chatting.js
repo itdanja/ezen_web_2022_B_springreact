33c2112b5a1b2c3711bcd402e80108b28ca22ae2
이젠강사 <itdanja@kakao.com>
2023-05-03 12:23:45 +0900

    day85 기본 메시지채팅

--- src/main/frontend/src/component/board/Chatting.js
+++ src/main/frontend/src/component/board/Chatting.js
@@ -3,28 +3,63 @@ import Container from '@mui/material/Container';
 
 export default function Chatting(props){
 
+    let [ msgContent , setMsgContent ] = useState([]); // 현재 채팅중인 메시지 를 저장하는 변수
+    let msgInput = useRef(null); // 채팅입력창[input] DOM객체 제어 변수
+
+    // 1. 재렌더링 될때마다 새로운 접속
+    // let 클라이언트소켓 = new WebSocket("ws/localhost:8080/chat")  ;
+    // 2. 재렌더링 될때 데이터 상태 유지
+    let 클라이언트소켓 = useRef( new WebSocket("ws://localhost:8080/chat") ) ;
+    // 3. 서버소켓에 접속했을때 이벤트
+    클라이언트소켓.current.onopen = () => { console.log('서버 접속했습니다.'); }
+    // 3. 서버소켓에 나갔을때 이벤트
+    클라이언트소켓.current.onclose = (e) => { console.log('서버 나갔습니다.'); }
+    // 3. 서버소켓과 오류 발생했을때 이벤트
+    클라이언트소켓.current.onerror = (e) => { console.log('소켓 오류'); }
+    // 3. 서버소켓으로 부터 메시지 받았을때 이벤트
+    클라이언트소켓.current.onmessage = (e) => {
+        console.log('서버소켓으로 메시지 받음');  console.log( e);  console.log( e.data );
+        //msgContent.push(e.data);    // 배열에 내용 추가
+        //setMsgContent( [...msgContent] ); // 재 렌더링
+        setMsgContent( [...msgContent , e.data ] ); // 재 렌더링
+    }
+
+    //
+    const onSend = () =>{
+        // msgInput변수가 참조중인 <input ref={ msgInput } > 해당 input 를 DOM객체로 호출
+        let msg = msgInput.current.value;
+        클라이언트소켓.current.send( msg ); // 클라이언트가 서버에게 메시지 전송 [ .send( ) ]
+        msgInput.current.value = '';
+    }
+
+    return (<>
+        <Container>
+           <h6>익명 채팅방</h6>
+           <div className="chatContentBox">
+           {
+                msgContent.map( (m)=>{
+                    return(<><div> { m } </div></>)
+                })
+           }
+           </div>
+           <div className="chatInputBox">
+                <span> 익명94 </span>
+                <input ref={ msgInput } type="text" />
+                <button onClick={ onSend }>전송</button>
+           </div>
+        </Container>
+    </>)
+}
+/*
+    JSX : html + js     {   }
+
+
     let 숫자 = 10;                            // 지역변수 : 컴포넌트[함수] 호출 될때마다 초기화
     let 숫자2 = useRef(10);                   // 재렌더링시 초기값이 적용되지 않는 함수 [ 반환값 : 객체{ current : 값 } ]
-    let inputRef = useRef(null);            // document.querySelector vs useRef
-
-    const [ id , setId ] = useState(0); // set메소드 사용시 컴포넌트 재호출[재렌더링]
-
-/*    // 1. 웹소켓 = webSocket = JS
-    let webSocket = useRef( null );
-    // 2.
-    useEffect( () => {
-        if( !webSocket.current ){
-            webSocket.current = new webSocket('ws://localhost:8080/chat');
-            // 1. 웹소켓이 접속 성공했을때.
-            webSocket.current.onopen = () =>  { console.log(webSocket);  }
-        }
-    })
-*/
 
     console.log( 숫자 )
     console.log( 숫자2 )
     console.log( id )
-
     // 2. 난수 생성
     let randId = Math.floor( Math.random() * ( 9999 -1 ) +1 );
         // Math.floor( Math.random() * ( 최댓값 - 최소값 ) + 최소값 ); : 정수 1~1000
@@ -33,25 +68,4 @@ export default function Chatting(props){
 
     // setId( randId );
 
-    const onSend = () =>{
-        console.log( inputRef.current.value  )
-        console.log( document.querySelector('.msgInput').value )
-        inputRef.current.value = 3543;
-    }
-    return (<>
-        <Container>
-           <h6>익명 채팅방</h6>
-           <div className="chatContentBox">
-
-           </div>
-           <div className="chatInputBox">
-                <span> 익명94 </span>
-                <input ref={ inputRef } type="text" className="msgInput" />
-                <button onClick={ onSend }>전송</button>
-           </div>
-        </Container>
-    </>)
-}
-/*
-    JSX : html + js     {   }
 */
--- src/main/java/ezenweb/web/socket/ChattingHandler.java
+++ src/main/java/ezenweb/web/socket/ChattingHandler.java
@@ -7,21 +7,33 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component // 빈 등록 [ 스프링 해당 클래스를 관리 = 제어역전 IOC ]
 @Slf4j // 로그
 public class ChattingHandler extends TextWebSocketHandler {
+    // 0. ** 서버소켓에 접속한 세션 명단 저장
+    private static List<WebSocketSession> 접속명단 = new ArrayList<>();
+
     @Override   // 1. 클라이언트가 서버소켓으로 부터 접속했을때
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("afterConnectionEstablished : " + session );
+        접속명단.add( session ); // 클라이언트 세션이 들어왔을때 리스트에 저장 [ 왜???다른 세션과 통신하기 위해 ]
     }
     @Override   // 2. 클라이언트로 부터 메시지를 받았을때
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         log.info("handleTextMessage : " + session );
         log.info("handleTextMessage : " + message );
+
+        for( WebSocketSession 세션 : 접속명단 ){ 세션.sendMessage( message );  }
+
     }
     @Override   // 3. 클라이언트가 서버소켓으로 부터 나갔을때
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info("afterConnectionClosed : " + session );
         log.info("afterConnectionClosed : " + status );
+        접속명단.remove( session ); // 클라이언트 세션이 나갔을때 리스트에 제거
     }
 }
