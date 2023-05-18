30365fe11262d8c05e4a685e9431cbf711c20ca1
이젠강사 <itdanja@kakao.com>
2023-05-18 13:38:17 +0900

    day91 배포

--- src/main/frontend/src/example/ex1component/ProductList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Product from './Product' // 컴포넌트 호출
-export default function ProductList(){
-    return ( <>
-        <Product name ='코카콜라' price = '1000' />
-        <Product name ='사이다' price = '500' />
-        <Product name ='환타' price = '700' />
-    </>);
-}
